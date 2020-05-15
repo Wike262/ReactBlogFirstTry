7f30d9fd36acc6159dac057e2a22a61633e747e7
Wike262 <kirill.samusenko.00@mail.ru>
2020-05-15 03:57:19 +0300

    Issue fixing nonexistent post display

--- src/components/errors/404/404.js
+++ src/components/errors/404/404.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import './404.sass'
 
-const error404 = () => {
+export default () => {
  return (
   <div className="Page-404">
-
    <h1>Oops.... Looks it's nothing there.</h1>
    <img src="https://firebasestorage.googleapis.com/v0/b/my-app-dd6a6.appspot.com/o/Images%2Fshutterstock_325494917-5a68d8403418c600190a3e1f.jpg?alt=media&token=5d5382ce-15b0-4da7-827f-b76cc3cf8190" alt="" />
   </div>
-
  )
 }
-
-export default error404;
--- src/pages/single_post/single.js
+++ src/pages/single_post/single.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
+import Error404 from '../../components/errors/404/404'
 import Post from '../../components/post/post'
 
 export default (props) => {
- return (
-  <div className="Post-SingleWrapper">
-   <div className="Post-Single Single">
-    <Post post={props.location.state.post} authorID={props.location.state.author} postID={props.match.params.postID} modClass='Post-SingleContent' />
+ if (!!props.location.state && !!props.location.state.author && !!props.match.params.postID) {
+  return (
+   <div className="Post-SingleWrapper">
+    <div className="Post-Single Single">
+     <Post post={props.location.state.post} authorID={props.location.state.author} postID={props.match.params.postID} modClass='Post-SingleContent' />
+    </div>
    </div>
-  </div>
- )
+  )
+ }
+ else {
+  return (<Error404 />)
+ }
 }
 
 
